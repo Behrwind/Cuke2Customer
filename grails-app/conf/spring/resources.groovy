dde644349a8ac5bd71c158b1dbf3e971d3933a04
Stefan Winterfeldt <stefan.winterfeldt@outlook.de>
2015-10-17 15:29:45 +0200

    prepared integration of git by renaming vcs configuration options and adding validation to them

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,27 +1,42 @@
 import grails.util.Holders
 import it.bitz.cuke2customer.JiraService
 import it.bitz.cuke2customer.SvnAdapterService
+import it.bitz.cuke2customer.enums.VCSIntegration
 
 // Place your Spring DSL code here
 beans = {
 
     def grailsApplication = Holders.grailsApplication
 
-    if(grailsApplication.config.project.svn.integration.toUpperCase() == 'ON') {
-        versionControlAdapter(SvnAdapterService) {
-            svnUser = grailsApplication.config.project.svnUser
-            svnPassword = grailsApplication.config.project.svnPassword
-            svnUrl = grailsApplication.config.project.svnUrl
-        }
+    VCSIntegration vcsIntegration
+
+    try {
+        vcsIntegration = VCSIntegration.valueOf (grailsApplication.config.project.vcs.integration)
+    } catch (IllegalArgumentException e) {
+        log.error ("${grailsApplication.config.project.vcs.integration} is not a valid option for project.vcs.integration, vcs integration will be set to OFF")
+        vcsIntegration = VCSIntegration.OFF
     }
 
-    if(grailsApplication.config.project.jira.integration.toUpperCase() == 'ON') {
-        jiraService(JiraService) {
+    switch (vcsIntegration) {
+        case VCSIntegration.SVN:
+            versionControlAdapter (SvnAdapterService) {
+                svnUser = grailsApplication.config.project.vcsUser
+                svnPassword = grailsApplication.config.project.vcsPassword
+                svnUrl = grailsApplication.config.project.vcsUrl
+            }
+            break
+        case VCSIntegration.OFF:
+            break
+        default:
+            log.error ("the vcs integration option ${vcsIntegration.name ()} cannot be handled yet")
+    }
+
+    if (grailsApplication.config.project.jira.integration.toUpperCase () == 'ON') {
+        jiraService (JiraService) {
             jiraUser = grailsApplication.config.project.jiraUser
             jiraPassword = grailsApplication.config.project.jiraPassword
             jiraUrl = grailsApplication.config.project.jiraUrl
         }
     }
-
 
 }
